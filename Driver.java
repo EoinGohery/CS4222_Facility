e2e00a7fce399000662930dba80d6c67762f9f1a
Eoin Gohery <eoingohery@gmail.com>
2018-04-08 13:55:46 +0100

    read

--- Driver.java
+++ Driver.java
@@ -10,6 +10,12 @@ public class Driver
   private static final String[] facilities = { };
   private static boolean loggedIn = false;
   private static boolean admin = false;
+  private static File userInfo = new File ("userInfo.txt");
+  private static File bookingInfo = new File ("bookings.txt");
+  private static File facilityInfo = new File ("facilities.txt");
+  public static List<Facility> facilities = new ArrayList<Facility>();
+  public static List<User> users = new ArrayList<User>();
+  public static List<Booking> bookings = new ArrayList<Booking>();
 
   public static void main(String[] args) throws IOException
   {
@@ -62,7 +68,8 @@ public class Driver
      }
    } else
    {
-     while (main) {
+     while (main)
+     {
        String section = (String) JOptionPane.showInputDialog(null, "Menu","",JOptionPane.QUESTION_MESSAGE, null, mainUser, mainUser[0]);
        if(section=="View Bookings") {
            viewBookings();
@@ -78,17 +85,78 @@ public class Driver
 
   public static void readUser()
   {
-
+    Scanner read;
+		String lineFromFile;
+    String FileElements[];
+    int userID;
+    String email;
+    int userType;
+    String password;
+		read = new Scanner(userInfo);
+		while(read.hasNext())
+		{
+			lineFromFile = read.nextLine();
+      fileElements = lineFromFile.split(",");
+      userID = (Integer.parseInt(fileElements[0]));
+      email = (fileElements[1])
+      password = (fileElements[2])
+      userType = (Integer.parseInt(fileElements[3]));
+      User tempUser = new User(userID, email, password, userType);
+      users.add(tempUser);
+		}
+		read.close();
   }
 
   public static void readBooking()
   {
-
+    Scanner read;
+		String lineFromFile;
+    String FileElements[];
+    int bookingID;
+    int facilityID;
+    int userID;
+    int slot;
+    String date;
+    String paymentStatus;
+		read = new Scanner(userInfo);
+		while(read.hasNext())
+		{
+			lineFromFile = read.nextLine();
+      fileElements = lineFromFile.split(",");
+      bookingID = (Integer.parseInt(fileElements[0]));
+      facilityID = (Integer.parseInt(fileElements[1]));
+      userID = (Integer.parseInt(fileElements[2]));
+      slot = (Integer.parseInt(fileElements[3]));
+      date = (fileElements[4])
+      paymentStatus = (fileElements[5])
+      Booking tempBooking = new Booking(bookingID, facilityID, userID, slot, date, paymentStatus);
+      bookings.add(tempBooking);
+		}
+		read.close();
   }
 
   public static void readFacility()
   {
-
+    Scanner read;
+		String lineFromFile;
+    String FileElements[];
+    int facilityID;
+    String facilityName;
+    double pricePerHour;
+    String decommissionedUntilDate;
+		read = new Scanner(userInfo);
+		while(read.hasNext())
+		{
+			lineFromFile = read.nextLine();
+      fileElements = lineFromFile.split(",");
+      facilityID = (Integer.parseInt(fileElements[0]));
+      facilityName = (fileElements[1]);
+      pricePerHour = (Double.parseDouble(fileElements[2]));
+      decommissionedUntilDate = (fileElements[3])
+      Booking tempFacility = new Facility(facilityID, facilityName, pricePerHour, decommissionedUntilDate);
+      facilities.add(tempFacility);
+		}
+		read.close();
   }
 
   public static void createUser()
